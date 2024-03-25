e8c64393f7533cf46e8aa640a8613ea182b039e9
zeelHM <zeel.hashmob@gmail.com>
2024-03-25 13:05:38 +0530

    new folder structure

--- index.js
+++ index.js
@@ -1,21 +1,48 @@
-import express from "express"
-import cors from "cors"
-import cookieParser from "cookie-parser"
-import dotenv from "dotenv"
-import connectDB from "./src/db/connection.js"
-import { app } from "./src/app.js"
+import express from "express";
+import cors from "cors";
+import cookieParser from "cookie-parser";
+import dotenv from "dotenv";
+import connectDB from "./db/connection.js";
+
 dotenv.config({
-    path: './.env'
+  path: "./.env",
+});
+
+ 
+
+const app = express();
+app.use(cors())
+
+app.use(express.json({limit: "16kb"}))
+app.use(express.urlencoded({extended: true, limit: "16kb"}))
+app.use(express.static("public"))
+app.use(cookieParser())
+
+ 
+
+
+// //routes import
+import userRouter from './route/user.route.js'
+
+
+//routes declaration
+app.use('/api/v1/users', userRouter)
+
+
+
+export { app }
+app.get('/',(req,res) =>{
+    res.send("hello world")
 })
+
+
 
 connectDB()
-.then(() => {
+  .then(() => {
     app.listen(process.env.PORT || 8000, () => {
-        console.log(`⚙️ Server is running at port : ${process.env.PORT}`);
-    })
-})
-.catch((err) => {
+      console.log(`⚙️ Server is running at port : ${process.env.PORT}`);
+    });
+  })
+  .catch((err) => {
     console.log("MONGO db connection failed!!! ", err);
-})
-
-
+  });
